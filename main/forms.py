0702c179e4f6a1f8631333149fa1ea12648c9b00
ekatfil <ekaterina.romanovna_2011@mail.ru>
2025-01-26 18:44:08 +0300

    styles

--- main/forms.py
+++ main/forms.py
@@ -11,7 +11,16 @@ class UserRequestForm(forms.ModelForm):
 
 
 class RegisterForm(UserCreationForm):
-    email = forms.EmailField(required=True)
+    email = forms.EmailField(
+        required=True, widget=forms.EmailInput(attrs={"class": "form-control"})
+    )
+    username = forms.CharField(widget=forms.TextInput(attrs={"class": "form-control"}))
+    password1 = forms.CharField(
+        widget=forms.PasswordInput(attrs={"class": "form-control"})
+    )
+    password2 = forms.CharField(
+        widget=forms.PasswordInput(attrs={"class": "form-control"})
+    )
 
     class Meta:
         model = User
